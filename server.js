8ea71a6184608c1e058aa9a32c8c74758756c83f
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-03-08 19:18:07 +0100

    Add server & endpoints

--- server.js
+++ server.js
@@ -3,7 +3,30 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: '988950317829-m2heug1o4sk7v1v59ddhcsv60qubpsm9.apps.googleusercontent.com',
+  clientSecret: 'IBWgnjH3QzjntMJMOpDTI-Ao',
+  callbackURL: 'http://localhost:8000/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+  console.log(profile)
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -12,6 +35,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
 
 app.get('/', (req, res) => {
   res.render('index');
